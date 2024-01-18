7fb6c81ff71b9b5da42d65a2004c7726476a7c26
kiprono <im.kiprono@gmail.com>
2024-01-18 15:16:10 +0300

    header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg'
 import './navbar.css'; 
 
+const Menu = () => (
+  <>
+  
+  <p><a href="#home">Home</a></p>
+         
+
+         <p><a href="#zanah">What is Zanah</a></p>
+
+         <p><a href="#possibility">Tech Solutions</a></p>
+
+         <p><a href="#features">Features</a></p>
+         <p><a href="#blog"></a>Research</p>
+  
+  </>
+)
+
 const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='zanah__navbar'>
       <div className='zanah__navbar-links'>
@@ -13,25 +32,38 @@ const Navbar = () => {
 
         </div>
         <div className='zanah__navbar-links_container'>
-          <p><a href="#home">Home</a></p>
-         
-
-          <p><a href="#zanah">What is Zanah</a></p>
-
-          <p><a href="#possibility">Tech Solutions</a></p>
-
-          <p><a href="#features">Features</a></p>
-          <p><a href="#blog"></a>Research</p>
-
-
-
+       <Menu />
 
         </div>
 
 
       </div>
 
-      
+      <div className="zanah__navbar-sign">
+      <p>Sign In</p>
+        <button type='button'>Sign Up</button>
+
+      </div>
+        <     div className='zanah__navbar-menu'>
+     {toggleMenu
+     ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)}/>
+     : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)}/>
+     
+    } 
+    {toggleMenu && (
+      <div className='zanah__navbar-menu_container scale-up-center'>
+        <div className='zanah__navbar-menu_container-links'>
+          <Menu/>
+          <div className="zanah__navbar-menu_container-links-sign">
+        <p>Sign In</p>
+              <button type='button'>Sign Up</button>
+
+      </div>
+        </div>
+        </div>
+    )}
+    </div>
+    
     </div>
   )
 }
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './header.css'; 
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='zanah_header section__padding' id='home'>
+      <div className='zanah__header-content'>
+        <h1 className='gradient__text'>Let's Build Something amazing at Zanah Technology</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <div className='zanah__header-content__input'>
+
+      <input type="email" placeholder='Your email address' />
+      <button type='button '>Get Started</button>
+      </div>
+
+      <div className="zanah__header-content__people">
+        <img src={people} />
+      </div>
+      </div>
+      
     </div>
   )
 }

55ffbd5fd9bbf3fcbd7f00d648039978c5787785
kiprono <im.kiprono@gmail.com>
2024-01-18 18:29:21 +0300

    brand component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './brand.css'; 
+import { google, slack, atlassian, shopify, dropbox } from './imports';
 
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div  className='zanah__brand section__padding'>
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlasian" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
     </div>
   )
 }
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,26 +7,30 @@ const Header = () => {
   return (
     <div className='zanah_header section__padding' id='home'>
       <div className='zanah__header-content'>
-        <h1 className='gradient__text'>Let's Build Something amazing at Zanah Technology</h1>
-        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      
+
+        <h1 className='gradient__text'>Let's Build Something amazing at Zanah Technology</h1> 
+       <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      
       <div className='zanah__header-content__input'>
 
-      <input type="email" placeholder='Your email address' />
-      <button type='button '>Get Started</button>
+         <input type="email" placeholder='Your email address' />
+         <button type='button '>Get Started</button>
       </div>
 
       <div className="zanah__header-content__people">
-        <img src={people} alt='people' />
-        <p>1,600 people requesed access visit in the last 24 hrs </p>
+          <img src={people} alt='people' />
+           <p>1,600 people requesed access visit in the last 24 hrs </p>
       </div>
-     
-
-      <div className="zanah__header-image">
-      <img src={ai} alt='ai' />
       </div>
 
       
+      <div className="zanah__header-image">
+      <img src={ai} alt='ai' />
       </div>
+      
+
+     
       
     </div>
   )
